b947f067f6d4354fb5b53a30e2d8d4ad13e088ab
Stanislav <Stanislav.Petrov@russianpost.ru>
2025-08-31 17:49:55 +0400

    3

--- src/main/java/ru/otus/java/HttpServer.java
+++ src/main/java/ru/otus/java/HttpServer.java
@@ -2,11 +2,15 @@ package ru.otus.java;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer {
     private int port;
     private Dispatcher dispatcher;
+    private volatile boolean running = true;
 
+    private final ExecutorService service = Executors.newFixedThreadPool(3);
     public HttpServer(int port) {
         this.port = port;
         this.dispatcher = new Dispatcher();
@@ -15,16 +19,25 @@ public class HttpServer {
     public void start() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Сервер запущен на порту: " + port);
-            try (Socket socket = serverSocket.accept()) {
-                byte[] buffer = new byte[8192];
-                int n = socket.getInputStream().read(buffer);
-                String rawRequest = new String(buffer, 0, n);
-                HttpRequest request = new HttpRequest(rawRequest);
-                request.info(true);
-                dispatcher.execute(request, socket.getOutputStream());
+            while (running) {
+                service.submit(()->{
+                    try(Socket socket = serverSocket.accept()){
+                        byte[] buffer = new byte[8192];
+                        int n = socket.getInputStream().read(buffer);
+                        String rawRequest = new String(buffer, 0, n);
+                        HttpRequest request = new HttpRequest(rawRequest);
+                        request.info(true);
+                        dispatcher.execute(request, socket.getOutputStream());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+
             }
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            service.shutdown();
         }
     }
 }
